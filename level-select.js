ce6e8d1368b4f2e1429224b26625961397ef20f0
mez50 <merenschannel@gmail.com>
2026-01-30 13:05:00 -0800

    Delete level-select.js

--- level-select.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.level-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-        const level = btn.dataset.level;
-        window.location.href = `game.html?level=${level}`;
-    });
-});
